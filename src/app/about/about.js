89a06e9f79817e3d2f82202344fde8946e97c65d
Josh David Miller <josh@joshdmiller.com>
2013-02-28 14:04:26 -0800

    feat(about): Added placeholders demo to about page
    Removed comments from about.js as they are covered in other README
    files.

--- src/app/about/about.js
+++ src/app/about/about.js
@@ -1,28 +1,9 @@
-/**
- * Each section of the site has its own module. It probably also has
- * submodules, though this boilerplate is too simple to aboutnstrate it. Within
- * `src/app/about`, however, could exist several additional folders representing
- * additional modules that would then be listed as dependencies of this one.
- * For example, a `note` section could have the submodules `note.create`,
- * `note.delete`, `note.edit`, etc.
- *
- * Regardless, so long as dependencies are managed correctly, the build process
- * will automatically take take of the rest.
- *
- * The dependencies block here is also where component dependencies should be
- * specified, as shown below.
- */
 angular.module( 'ngBoilerplate.about', [
   'placeholders',
   'ui.bootstrap',
   'titleService'
 ])
 
-/**
- * Each section or module of the site can also have its own routes. AngularJS
- * will handle ensuring they are all available at run-time, but splitting it
- * this way makes each module more "self-contained".
- */
 .config([ '$routeProvider', function config( $routeProvider ) {
   $routeProvider.when( '/about', {
     controller: 'AboutCtrl',
@@ -30,9 +11,6 @@ angular.module( 'ngBoilerplate.about', [
   });
 }])
 
-/**
- * And of course we define a controller for our route.
- */
 .controller( 'AboutCtrl', [ '$scope', 'titleService', function AboutCtrl( $scope, titleService ) {
   titleService.setTitle( 'What is It?' );
   
