f65864ecd3d566ef2ef0271ba08d06c3bd0b8b6d
Josh David Miller <josh@joshdmiller.com>
2013-02-28 16:47:59 -0800

    Updated Google+ button (again)

--- assets/ng-boilerplate.js
+++ assets/ng-boilerplate.js
@@ -1900,15 +1900,18 @@ angular.module("home/home.tpl.html", []).run(["$templateCache", function($templa
     "    </li>" +
     "    <li>" +
     "      <!-- Place this tag where you want the +1 button to render. -->" +
-    "      <div class=\"g-plusone\" data-size=\"medium\" data-annotation=\"inline\" data-width=\"300\" data-href=\"http://bit.ly/ngBoilerplate\"></div>" +
+    "      <div class=\"g-plusone\" data-size=\"medium\"></div>" +
     "" +
     "      <!-- Place this tag after the last +1 button tag. -->" +
     "      <script type=\"text/javascript\">" +
-    "        (function() {" +
-    "          var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;" +
-    "          po.src = 'https://apis.google.com/js/plusone.js';" +
-    "          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);" +
-    "        })();" +
+    "          (function () {" +
+    "              var po = document.createElement('script');" +
+    "              po.type = 'text/javascript';" +
+    "              po.async = true;" +
+    "              po.src = 'https://apis.google.com/js/plusone.js';" +
+    "              var s = document.getElementsByTagName('script')[0];" +
+    "              s.parentNode.insertBefore(po, s);" +
+    "          })();" +
     "      </script>" +
     "    </li>" +
     "  </ul> " +
