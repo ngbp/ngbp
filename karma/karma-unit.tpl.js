89acf5f6b6b3a7ee4508c0ea309655215d7b05d1
Rick Huizinga <rickhuizinga@gmail.com>
2013-09-13 15:00:27 -0700

    feat(karma): Added PhantomJS support.
    Change the Karma browsers config and install PhantomJS by running 'npm
    install --save-dev phantomjs' and seting the PHANTOMJS_BIN environment
    variable.

--- karma/karma-unit.tpl.js
+++ karma/karma-unit.tpl.js
@@ -18,7 +18,7 @@ module.exports = function ( karma ) {
       'src/assets/**/*.js'
     ],
     frameworks: [ 'jasmine' ],
-    plugins: [ 'karma-jasmine', 'karma-firefox-launcher', 'karma-chrome-launcher', 'karma-coffee-preprocessor' ],
+    plugins: [ 'karma-jasmine', 'karma-firefox-launcher', 'karma-chrome-launcher', 'karma-phantomjs-launcher', 'karma-coffee-preprocessor' ],
     preprocessors: {
       '**/*.coffee': 'coffee',
     },
