51de28c1a3facdcf8ee84de2352663a9ff3df93e
Josh David Miller <josh@joshdmiller.com>
2014-05-14 15:16:50 -0700

    feat(karma): Added Karma spell

---
+++ karma.conf.js
@@ -0,0 +1,21 @@
+module.exports = function ( karma ) {
+  karma.set({
+    /**
+     * The list of browsers to launch to test on. This includes only "Firefox" by
+     * default, but other browser names include:
+     * Chrome, ChromeCanary, Firefox, Opera, Safari, PhantomJS
+     *
+     * Note that you can also use the executable name of the browser, like "chromium"
+     * or "firefox", but that these vary based on your operating system.
+     *
+     * You may also leave this blank and manually navigate your browser to
+     * http://localhost:9018/ when you're running tests. The window/tab can be left
+     * open and the tests will automatically occur there during the build. This has
+     * the aesthetic advantage of not launching a browser every time you save.
+     */
+    browsers: [
+      'Firefox'
+    ]
+  });
+};
+
